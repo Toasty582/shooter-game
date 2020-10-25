2dcf75444f0a60cce8105ae353eabe23d4df9db3
Jonas Wester <jonas@wester.org>
2020-10-25 17:02:28 +0100

    Created basic movement

---
+++ Shooter-Game/Assets/Scripts/PlayerCameraBehavior.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerCameraBehavior : MonoBehaviour
+{
+    public Camera playerCamera;
+    public GameObject player;
+    Vector3 cameraOffset = new Vector3(0f, 0.5f, 0f);
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerCamera.transform.position = player.transform.position + cameraOffset;
+    }
+}
---
+++ Shooter-Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody playerRigid;
+    public float forceApplied;
+    public float topSpeed;
+
+    Vector2 direction;
+
+    private void FixedUpdate() {
+        Debug.Log(direction);
+
+        playerRigid.AddRelativeForce(direction.x * forceApplied * Time.deltaTime, 0f, direction.y * forceApplied * Time.deltaTime);
+
+        if (playerRigid.velocity.magnitude > topSpeed) {
+            playerRigid.velocity = playerRigid.velocity.normalized * topSpeed;
+        }
+    }
+    public void Move(InputAction.CallbackContext context) {
+        direction = context.ReadValue<Vector2>();
+    }
+}
