565dc519c2205f20e947330e9ce3412a84956df6
Jonas Wester <jonas@wester.org>
2020-10-26 15:46:21 +0100

    created foundation for single-file save system

---
+++ Shooter-Game/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Player : MonoBehaviour
+{
+    public Slider healthBar;
+    public TextMeshProUGUI healthText;
+
+    public int health = 100;
+    public int sceneIndex = 0;
+
+    public static Player player;
+    private void Awake() {
+        if (player != null && player != this) {
+            Destroy(this.gameObject);
+        } else {
+            player = this;
+        }
+    }
+
+    public void TakeDamage(int amount) {
+        health -= amount;
+        healthBar.value = health / 100;
+        healthText.text = health + " / 100";
+    }
+
+    public void SavePlayer() {
+        SaveSystem.SavePlayer(this);
+    }
+
+    public void LoadPlayer() {
+        PlayerData data = SaveSystem.LoadPlayer();
+
+        health = data.health;
+        sceneIndex = data.sceneIndex;
+
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+    }
+}
---
+++ Shooter-Game/Assets/Scripts/Using/UseLogic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseLogic : MonoBehaviour
+{
+    private void OnEnable() {
+        EventHandler.OnUse += Use;
+    }
+
+    private void OnDisable() {
+        EventHandler.OnUse -= Use;
+    }
+
+    void Use(GameObject target) {
+        // Checks if the used object is this object
+        if (gameObject == target) {
+            // log that the object has been used
+            Debug.Log(gameObject.name + " has been used!");
+        }
+    }
+}
