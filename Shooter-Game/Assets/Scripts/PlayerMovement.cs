d5f7069203501ab7bc7f4f8656afc3970c167d3a
Jonas Wester <jonas@wester.org>
2020-10-26 09:58:40 +0100

    added basic use system

--- Shooter-Game/Assets/Scripts/PlayerMovement.cs
+++ Shooter-Game/Assets/Scripts/PlayerMovement.cs
@@ -15,13 +15,16 @@ public class PlayerMovement : MonoBehaviour
     Vector2 direction;
 
     private void Start() {
+        // Defines the player and the collider size
         player = playerRigid.gameObject;
         distToGround = player.GetComponent<BoxCollider>().bounds.extents.y;
     }
 
     private void Update() {
+        // Offsets the playerLook object
         playerLook.transform.position = playerRigid.gameObject.transform.position + new Vector3(0f, 1f, 0f);
 
+        // Rotates the y rotation to the playerlook rotation
         playerRigid.gameObject.transform.eulerAngles = new Vector3(
             playerRigid.gameObject.transform.eulerAngles.x,
             playerLook.transform.eulerAngles.y,
@@ -30,8 +33,10 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void FixedUpdate() {
+        // Adds force based on the move value
         playerRigid.AddRelativeForce(direction.x * forceApplied * Time.fixedDeltaTime, 0f, direction.y * forceApplied * Time.fixedDeltaTime);
 
+        // Sets max speed
         if (playerRigid.velocity.magnitude > topSpeed) {
             playerRigid.velocity = playerRigid.velocity.normalized * topSpeed;
         }
@@ -41,7 +46,9 @@ public class PlayerMovement : MonoBehaviour
     }
 
     public void Jump(InputAction.CallbackContext context) {
+        // Checks if player is grounded
         if (Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f)) {
+            // Applies an upward force
             playerRigid.AddForce(Vector3.up * 250f);
         }
     }
---
+++ Shooter-Game/Assets/Scripts/UseLogic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseLogic : MonoBehaviour
+{
+    public delegate void UseAction();
+    public UseAction useAction;
+    
+    private void OnEnable() {
+        EventHandler.OnUse += Use;
+    }
+
+    private void OnDisable() {
+        EventHandler.OnUse -= Use;
+    }
+
+    void Use(GameObject target) {
+        // Checks if the used object is this object
+        if (gameObject == target) {
+            // log that the object has been used
+            Debug.Log(gameObject.name + " has been used!");
+            // Call the function that has been assigned to this instance of the script
+            useAction();
+        }
+    }
+}
